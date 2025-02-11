3c6764da03a00f1855d6e496927a4efe296dbe1f
kavinbharathii <r.m.kavinbharathi@gmail.com>
2025-02-11 21:51:24 +0530

    [Unsolved] Added lookup for faster results

--- day11/main.py
+++ day11/main.py
@@ -2,38 +2,46 @@ import sys
 
 if len(sys.argv) == 1:
     with open("data.aoc", "r") as file:
-        data = [n for n in file.readline().split(' ')]
+        data = [n for n in file.readline().strip().split(' ')]
         file.close()
 else:
     if sys.argv[1] == 'test':
         with open("test.aoc", "r") as file:
-            data = [n for n in file.readline().split(' ')]
+            data = [n for n in file.readline().strip().split(' ')]
             file.close()
 
     else:
         with open("data.aoc", "r") as file:
-            data = [n for n in file.readline().split(' ')]
+            data = [n for n in file.readline().strip().split(' ')]
             file.close()
 
 # ------------------------------------------------------------------------------------------------------------------- #
 
-next_blink = []
+next_blink = ""
+lookup = dict()
 
 for blink in range(75):
-    print(blink + 1, len(data))
+    print(blink, len(data), " ".join(data[:5]))
     for num in data:
-        if num == '0':
-            next_blink.append('1')
-        elif len(num) % 2 == 0:
-            l, r = num[:len(num) // 2], num[len(num) // 2:]
-            next_blink.append(l)
-            next_blink.append(r.lstrip('0') or '0')
+        if num in lookup.keys():
+            next_blink += lookup[num] + " "
         else:
-            stone = int(num) * 2024
-            next_blink.append(str(stone))
+            if num == '0':
+                result = '1'
+            elif len(num) % 2 == 0:
+                l, r = num[:len(num) // 2], num[len(num) // 2:]
+                result = l
+                result += " " + (r.lstrip('0') or '0')
+            else:
+                stone = int(num) * 2024
+                result = str(stone)
+    
+            lookup[num] = result
+            next_blink += result + " "
 
-    data = next_blink
-    next_blink = []
+    next_blink = next_blink.strip(" ")
+    data = next_blink.split(" ")
+    next_blink = ""
 
 print(len(data))
 
